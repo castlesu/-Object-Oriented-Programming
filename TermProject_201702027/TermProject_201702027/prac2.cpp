85f3ce0d9dbbb1891e956cef6129de49d4bafb70
castlesu <shsj4952@naver.com>
2020-12-12 20:31:24 +0900

    deleteWord 수정

--- TermProject_201702027/TermProject_201702027/prac2.cpp
+++ TermProject_201702027/TermProject_201702027/prac2.cpp
@@ -183,9 +183,9 @@ tuple<vector<string>, bool> changeWord(vector<string> file, string ori, string w
     return tuple<vector<string>, bool>(file, isChange);
 }
 tuple<vector<string>,bool> insertWord(vector<string> file, int col, int row, string iword, int currentPage) {
-    int wantrow = currentPage + col - 1;
+    int wantcol = currentPage + col - 1;
 
-    stringstream tmp(file[wantrow]);
+    stringstream tmp(file[wantcol]);
     string input = tmp.str();
     string delimiter = " ";
     int chk = 1;
@@ -211,10 +211,10 @@ tuple<vector<string>,bool> insertWord(vector<string> file, int col, int row, str
     if (chk < row) {
         isInsert = false;
     }
-    file[wantrow] = current;
+    file[wantcol] = current;
     return tuple<vector<string>,bool>(file,isInsert);
 }
-vector<string> DeleteWord(vector<string> file, int col, int row, int currentPage) {
+tuple<vector<string>, bool> DeleteWord(vector<string> file, int col, int row, int currentPage) {
     int wantrow = currentPage + col - 1;
 
     stringstream tmp(file[wantrow]);
@@ -224,7 +224,7 @@ vector<string> DeleteWord(vector<string> file, int col, int row, int currentPage
     string token;
     size_t pos = 0;
     string current;
-
+    bool isDelete = false;
     while ((pos = input.find(delimiter)) != string::npos) {
         token = input.substr(0, pos);
         string tmp = token;
@@ -232,11 +232,14 @@ vector<string> DeleteWord(vector<string> file, int col, int row, int currentPage
         if (chk != row) {
             current.append(tmp + " ");
         }
+        else if(chk == row) {
+            isDelete = true;
+        }
         input.erase(0, pos + delimiter.length());
         chk += 1;
     }
     file[wantrow] = current;
-    return file;
+    return tuple<vector<string>, bool>(file, isDelete);
 }
 
 int main() {
@@ -357,13 +360,24 @@ int main() {
 
                 }
                 else {
-                    vecOffile = DeleteWord(vecOffile, col, row, currentPage - 20);
+                    bool isDelete;
+                    tuple<vector<string>, bool> deleteResult = DeleteWord(vecOffile, col, row, currentPage - 20);
+                    vecOffile = get<0>(deleteResult);
+                    isDelete = get<1>(deleteResult);
                     vecOffile = GlobalClass::instance()->SortFile(vecOffile);
                     tuple<int, bool> printResult = GlobalClass::instance()->CurrentPrint(vecOffile, currentPage - 20);
                     currentPage = get<0>(printResult);
-                    cout << "-------------------------------------------------------------------------------\n" << "n:다음페이지, p:이전페이지, i:삽입, d:삭제, c:변경, s:찾기, t:저장후 종료 \n" << "-------------------------------------------------------------------------------\n";
-                    cout << col << "번째 줄의 " << row << "번째 단어가 삭제되었습니다!" << endl;
-                    cout << "-------------------------------------------------------------------------------\n";
+                    if (isDelete) {
+                        cout << "-------------------------------------------------------------------------------\n" << "n:다음페이지, p:이전페이지, i:삽입, d:삭제, c:변경, s:찾기, t:저장후 종료 \n" << "-------------------------------------------------------------------------------\n";
+                        cout << col << "번째 줄의 " << row << "번째 단어가 삭제되었습니다!" << endl;
+                        cout << "-------------------------------------------------------------------------------\n";
+                    }
+                    else {
+                        cout << "-------------------------------------------------------------------------------\n" << "n:다음페이지, p:이전페이지, i:삽입, d:삭제, c:변경, s:찾기, t:저장후 종료 \n" << "-------------------------------------------------------------------------------\n";
+                        cout << col << "번째 줄의 "<< row << "번째 단어를 못찾았습니다! " << endl;
+                        cout << "-------------------------------------------------------------------------------\n";
+                    }
+                    
                 }
             }
             else if (chkPara.compare("s") == 0) {
